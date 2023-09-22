624dc8f97f4c54a45f5a64e6fd66961a47ed5a09
spyw4re <126114608+spyw4re@users.noreply.github.com>
2023-09-22 05:30:37 -0300

    Fixed comments placement.

--- CPUID/get_vendor_name.cpp
+++ CPUID/get_vendor_name.cpp
@@ -7,10 +7,10 @@ int main(int argc, char* argv[]) {
 
 	BOOL beingVirtualized;
 	int maximumFunction = 0;
-	int vendor[13];						//every vendor-id is a 12 char string, 13 = \0
+	int vendor[13];					//every vendor-id is a 12 char string, 13 = \0
 
 	__asm {
-		mov eax, 1						//leaf 1 stored in eax
+		mov eax, 1				//leaf 1 stored in eax
 		cpuid
 
 		test ecx, 0x80000000			//if MSB of ECX is equal to 1, then it is being virtualized
@@ -22,18 +22,18 @@ int main(int argc, char* argv[]) {
 	is_virtualized:
 		mov beingVirtualized, 1			//virtualization detected
 
-		xor eax, eax					//eax equals 0
-		mov eax, 0x40000000				//hypervisor brand(?) stored in  eax
+		xor eax, eax				//eax equals 0
+		mov eax, 0x40000000			//hypervisor brand(?) stored in  eax
 		cpuid
 
 		mov maximumFunction, eax		//up to what leaf number we can query
 		mov vendor[0], ebx
 		mov vendor[4], edx
 		mov vendor[8], ecx
-		mov byte ptr[vendor + 12], 0	/*each "piece" of vendor id is stored into a register.
-										So, we simply store those values onto vendor[] array.
-										Next, we manually put a NULL character for printing purposes.
-										*/
+		mov byte ptr[vendor + 12], 0		/*each "piece" of vendor id is stored into a register.
+							So, we simply store those values onto vendor[] array.
+							Next, we manually put a NULL character for printing purposes.
+							*/
 	done:
 	};
 
