24bc87fbaa8804148732d841f45d797e604c2088
Matt Hand <matt@matterpreter.com>
2023-09-28 10:02:57 -0400

    Update HardwareFeatures.cpp

--- CPUID/HardwareFeatures.cpp
+++ CPUID/HardwareFeatures.cpp
@@ -2,22 +2,16 @@
 #include <windows.h>
 #include <stdint.h>
 
-/* 
-This code actually don't work, i keep getting an EXCEPTION_ACCESS_VIOLATION whenever i execute it on any environment.
-Idk if im stupid or i really cannot access those hardware features provided by the Hypervisor, still searching about it.
-If you have any suggestions, feel free to reach me or make a PR. Have a nice day. 
-*/
-
 typedef struct HardwareFeatures {
-	BOOL SLAT;				//EAX[3]
+	BOOL SLAT;						//EAX[3]
 	BOOL InterruptRemapping;		//EAX[5]
-	BOOL DMAProtection;			//EAX[7]
+	BOOL DMAProtection;				//EAX[7]
 	uint32_t HypervisorLevel;		//((unsigned long)EAX >> 10) & 0x0F
-	BOOL PhysicalDestinationMode;		//EAX[14]
+	BOOL PhysicalDestinationMode;	//EAX[14]
 	BOOL UnrestrictedGuest;			//EAX[17]
 	BOOL ResourceMonitoring;		//EAX[19]
-	BOOL ACPIWDAT;				//EAX[24]
-} hardwarefeatures;
+	BOOL ACPIWDAT;					//EAX[24]
+} HardwareFeatures;
 
 HardwareFeatures GetHardwareFeatures();
 
@@ -66,88 +60,88 @@ HardwareFeatures GetHardwareFeatures() {
 		jmp done
 
 
-	is_virtualized :
-		mov beingVirtualized, 1
+		is_virtualized :
+			mov beingVirtualized, 1
 
-		xor eax, eax
-		mov eax, 0x40000000
-		cpuid
+			xor eax, eax
+			mov eax, 0x40000000
+			cpuid
 
-		cmp eax, 0x40000006
-		jge at_least_six
-		mov hardwareFeatures_enabled, 0
-		mov hardwarefeatures.SLAT, 0
-		mov hardwarefeatures.InterruptRemapping, 0
-		mov hardwarefeatures.DMAProtection, 0
-		mov hardwarefeatures.PhysicalDestinationMode, 0
-		mov hardwarefeatures.UnrestrictedGuest, 0
-		mov hardwarefeatures.ResourceMonitoring, 0
-		mov hardwarefeatures.ACPIWDAT, 0
-		mov hardwarefeatures.HypervisorLevel, 0
-		jmp done
-
-
-	at_least_six :
-		mov hardwareFeatures_enabled, 1
-		xor eax, eax
-		mov eax, 0x40000006
-		cpuid
-
-		test eax, 0x4
-		jnz SLAT_supported
-		mov hardwarefeatures.SLAT, 0
-
-	SLAT_supported :
-		mov hardwarefeatures.SLAT, 1
-
-		test eax, 0x20
-		jnz InterruptRemapping_supported
-		mov hardwarefeatures.InterruptRemapping, 0
-
-	InterruptRemapping_supported :
-		mov hardwarefeatures.InterruptRemapping, 1
-
-		test eax, 0x80
-		jnz DMAProtection_supported
-		mov hardwarefeatures.DMAProtection, 0
-
-	DMAProtection_supported :
-		mov hardwarefeatures.DMAProtection, 1
-
-		test eax, 0x4000
-		jnz PhysicalDestinationMode_supported
-		mov hardwarefeatures.PhysicalDestinationMode, 0
-
-	PhysicalDestinationMode_supported :
-		mov hardwarefeatures.PhysicalDestinationMode, 1
-
-		test eax, 0x20000
-		jnz UnrestrictedGuest_supported
-		mov hardwarefeatures.UnrestrictedGuest, 0
-
-	UnrestrictedGuest_supported :
-		mov hardwarefeatures.UnrestrictedGuest, 1
-
-		test eax, 0x80000
-		jnz ResourceMonitoring_supported
-		mov hardwarefeatures.ResourceMonitoring, 0
-
-	ResourceMonitoring_supported :
-		mov hardwarefeatures.ResourceMonitoring, 1
-
-		test eax, 0x1000000
-		jnz ACPIWDAT_supported
-		mov hardwarefeatures.ACPIWDAT, 0
-
-	ACPIWDAT_supported :
-		mov hardwarefeatures.ACPIWDAT, 1
-
-		shr eax, 10
-		and eax, 0x0F
-		mov hardwarefeatures.HypervisorLevel, eax
+			cmp eax, 0x40000006
+			jge at_least_six
+			mov hardwareFeatures_enabled, 0
+			mov hardwarefeatures.SLAT, 0
+			mov hardwarefeatures.InterruptRemapping, 0
+			mov hardwarefeatures.DMAProtection, 0
+			mov hardwarefeatures.PhysicalDestinationMode, 0
+			mov hardwarefeatures.UnrestrictedGuest, 0
+			mov hardwarefeatures.ResourceMonitoring, 0
+			mov hardwarefeatures.ACPIWDAT, 0
+			mov hardwarefeatures.HypervisorLevel, 0
+			jmp done
 
 
-	done :
+		at_least_six :
+			mov hardwareFeatures_enabled, 1
+			xor eax, eax
+			mov eax, 0x40000006
+			cpuid
+
+			test eax, 0x4
+			jnz SLAT_supported
+			mov hardwarefeatures.SLAT, 0
+
+		SLAT_supported :
+			mov hardwarefeatures.SLAT, 1
+
+			test eax, 0x20
+			jnz InterruptRemapping_supported
+			mov hardwarefeatures.InterruptRemapping, 0
+
+		InterruptRemapping_supported :
+			mov hardwarefeatures.InterruptRemapping, 1
+
+			test eax, 0x80
+			jnz DMAProtection_supported
+			mov hardwarefeatures.DMAProtection, 0
+
+		DMAProtection_supported :
+			mov hardwarefeatures.DMAProtection, 1
+
+			test eax, 0x4000
+			jnz PhysicalDestinationMode_supported
+			mov hardwarefeatures.PhysicalDestinationMode, 0
+
+		PhysicalDestinationMode_supported :
+			mov hardwarefeatures.PhysicalDestinationMode, 1
+
+			test eax, 0x20000
+			jnz UnrestrictedGuest_supported
+			mov hardwarefeatures.UnrestrictedGuest, 0
+
+		UnrestrictedGuest_supported :
+			mov hardwarefeatures.UnrestrictedGuest, 1
+
+			test eax, 0x80000
+			jnz ResourceMonitoring_supported
+			mov hardwarefeatures.ResourceMonitoring, 0
+
+		ResourceMonitoring_supported :
+			mov hardwarefeatures.ResourceMonitoring, 1
+
+			test eax, 0x1000000
+			jnz ACPIWDAT_supported
+			mov hardwarefeatures.ACPIWDAT, 0
+
+		ACPIWDAT_supported :
+			mov hardwarefeatures.ACPIWDAT, 1
+
+			shr eax, 10
+			and eax, 0x0F
+			mov hardwarefeatures.HypervisorLevel, eax
+
+
+			done :
 	};
 
 	if (beingVirtualized) {
